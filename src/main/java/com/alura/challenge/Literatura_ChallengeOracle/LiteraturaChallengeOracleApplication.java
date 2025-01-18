0120e9bf99cb018c2ea77406add9a7fdf2cce28e
J-DiegoSr <jdiego.sr96@gmail.com>
2025-01-18 12:03:42 -0500

    Inicializando Proyecto

---
+++ src/main/java/com/alura/challenge/Literatura_ChallengeOracle/LiteraturaChallengeOracleApplication.java
@@ -0,0 +1,13 @@
+package com.alura.challenge.Literatura_ChallengeOracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraturaChallengeOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraturaChallengeOracleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/challenge/Literatura_ChallengeOracle/LiteraturaChallengeOracleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.challenge.Literatura_ChallengeOracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraturaChallengeOracleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
